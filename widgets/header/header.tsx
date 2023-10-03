cc586011dd028ef3d719491de42d4128c64996b1
3str-webdev <3str.webdev@gmail.com>
2023-10-03 13:52:22 +0300

    feat: add header animation

--- widgets/header/header.tsx
+++ widgets/header/header.tsx
@@ -3,11 +3,14 @@ import { classes } from "@/shared/lib";
 export const Header = () => {
   return (
     <header
-      className={classes("fixed w-full flex gap-[10px] px-4", "lg:px-10")}
+      className={classes(
+        "fixed w-full flex gap-[10px] px-4 header-animation",
+        "lg:px-10"
+      )}
     >
       <div
         className={classes(
-          "text-primary-500 bg-neutral-1 text-h3 font-h3 px-5 py-4 backdrop-blur-md rounded-b-md",
+          "text-primary-500 bg-neutral-600/40 text-h3 font-h3 px-5 py-4 backdrop-blur-xl rounded-b-md",
           "md:text-h2 font-h2"
         )}
       >
@@ -16,7 +19,7 @@ export const Header = () => {
 
       <div
         className={classes(
-          "w-full hidden items-center justify-end px-5 py-4 bg-neutral-1 backdrop-blur-md rounded-b-md",
+          "w-full hidden items-center justify-end px-5 py-4 bg-neutral-600/40 backdrop-blur-xl rounded-b-md",
           "md:flex"
         )}
       >
