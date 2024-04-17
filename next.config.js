37269586bf057a1afe81d4344dcbf7cc6e94658a
3str-webdev <3str.webdev@gmail.com>
2024-04-17 23:04:46 +0300

    fix: comment useless proxy

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.API_URL}api/:path*`,
-      },
-      {
-        source: "/uploads/:path*",
-        destination: `${process.env.API_URL}uploads/:path*`,
-      },
-    ];
-  },
+  // rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `${process.env.API_URL}api/:path*`,
+  //     },
+  //     {
+  //       source: "/uploads/:path*",
+  //       destination: `${process.env.API_URL}uploads/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
