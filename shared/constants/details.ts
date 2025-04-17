825b14ccecb6896d98429bfd9a321d608ebd822a
sergey-frey <3str.webdev@gmail.com>
2025-04-17 10:38:09 +0300

    Update details.ts
    Актуализировал контакты

--- shared/constants/details.ts
+++ shared/constants/details.ts
@@ -1,7 +1,7 @@
 export const DETAILS = {
-	PORTFOLIO_GITHUB: "https://github.com/3str-webdev/3str.portfolio.client",
-	TELEGRAM_LINK: "https://t.me/serg_3str",
-	TELEGRAM_USERNAME: "@serg_3str",
+	PORTFOLIO_GITHUB: "https://github.com/sergey-frey/3str.portfolio.client",
+	TELEGRAM_LINK: "https://t.me/sergeyfrey",
+	TELEGRAM_USERNAME: "@sergeyfrey",
 	PHONE_NUMBER: "+79523606170",
-	EMAIL: "3str.webdev@gmail.com",
+	EMAIL: "sergey.frey.work@gmail.com",
 };
